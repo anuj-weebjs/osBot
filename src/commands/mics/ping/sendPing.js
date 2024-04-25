3df466eac7afc70a2e04d06946f1f4c6e741328c
anuj-weebjs <weeb.js@yahoo.com>
2024-04-25 15:52:56 +0530

    Included ram usage

--- src/commands/mics/ping/sendPing.js
+++ src/commands/mics/ping/sendPing.js
@@ -1,33 +1,37 @@
 const index = require('../../../index');
 const client = index.client;
+const memory = require('memory');
 const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
-    execute: async (message) =>{
-
+    execute: async (message) => {
+        const _memory = memory();
         const pingEmbed = new EmbedBuilder()
-        .setColor('#FF6347')
-        .setTitle('Pong!')
-        .setDescription('Loading...');
+            .setColor('#FF6347')
+            .setTitle('Pong!')
+            .setDescription('Loading...')
+            .addFields(
+                { name: 'Ram', value: `${_memory.toString()}mb`, inline: false }
+            );
 
-    message.channel.send({embeds: [pingEmbed]}).then(sentMessage => {
-        if (!sentMessage || !sentMessage.createdTimestamp || !message.createdTimestamp) {
-            console.log('Failded to calculate ping. Message or timestamps are undefined.')
-            pingEmbed.setDescription(`Failded to calculate ping`);
-            sentMessage.edit({embeds: [pingEmbed]});
-            return;
-        }
+        message.channel.send({ embeds: [pingEmbed] }).then(sentMessage => {
+            if (!sentMessage || !sentMessage.createdTimestamp || !message.createdTimestamp) {
+                console.log('Failed to calculate ping. Message or timestamps are undefined.')
+                pingEmbed.setDescription(`Failded to calculate ping`);
+                sentMessage.edit({ embeds: [pingEmbed] });
+                return;
+            }
 
-        const botLatency = sentMessage.createdTimestamp - message.createdTimestamp;
-        const apiLatency = client.ws.ping;
+            const botLatency = sentMessage.createdTimestamp - message.createdTimestamp;
+            const apiLatency = client.ws.ping;
 
-        pingEmbed.setDescription(`Bot latency: ${botLatency}ms\n API Latency: ${apiLatency}ms`);
-        pingEmbed.setFooter({text: 'requested by '+ message.author.tag, iconUrl: message.author.displayAvatarURL() });
-        pingEmbed.setTimestamp();
+            pingEmbed.setDescription(`Bot latency: ${botLatency}ms\n API Latency: ${apiLatency}ms`);
+            pingEmbed.setFooter({ text: 'requested by ' + message.author.tag, iconUrl: message.author.displayAvatarURL() });
+            pingEmbed.setTimestamp();
 
-        sentMessage.edit({embeds: [pingEmbed]});
-    }).catch(error =>{
-        console.log("Error sending or editing message: " + error);
-    });
+            sentMessage.edit({ embeds: [pingEmbed] });
+        }).catch(error => {
+            console.log("Error sending or editing message: " + error);
+        });
     },
 };
