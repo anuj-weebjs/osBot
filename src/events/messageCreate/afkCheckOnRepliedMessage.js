278d10acb304b6f87483416b3ccb2d376bb5e6c7
anuj-weebjs <weeb.js@yahoo.com>
2024-04-28 20:13:05 +0530

    fixed minor bug

--- src/events/messageCreate/afkCheckOnRepliedMessage.js
+++ src/events/messageCreate/afkCheckOnRepliedMessage.js
@@ -13,7 +13,7 @@ module.exports = {
                 console.log(err);
             }
 
-
+            if (queryResult) return;
             if (queryResult.reason) {
                 if (queryResult.length < 1) return;
 
