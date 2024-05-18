d105148c9710c5cac245c71c95fe76420f02dfdb
DatGamet <113996043+DatGamet@users.noreply.github.com>
2024-05-18 20:52:50 +0200

    sendServerInfo.ts: Added some changes ^^

--- src/commands/info/serverinfo/sendServerInfo.ts
+++ src/commands/info/serverinfo/sendServerInfo.ts
@@ -1,15 +1,17 @@
-var { EmbedBuilder } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
 module.exports = {
     execute: async (message: any) => {
         try {
             const guild = message.guild;
+            const owner = await guild.fetchOwner();
+            const createdTimestamp = parseInt(guild.createdAt / 1000);
             const ServerEmbed = new EmbedBuilder()
                 .setColor('#2B2D31')
                 .setTitle(`${guild.name} - Server info`)
                 .setThumbnail(guild.iconURL({ size: 256 }))
                 .setImage(guild.bannerURL({ size: 256 }))
                 .addFields(
-                    { name: 'Owner', value: (await guild.fetchOwner()).user.tag, inline: true },
+                    { name: 'Owner', value: `${owner.user.displayName} (${owner.user.id})`, inline: true },
                     {
                         name: 'Text Channels',
                         value: guild.channels.cache.filter((c: any) => c.type === 0).size.toString(),
@@ -27,9 +29,10 @@ module.exports = {
                     },
                     { name: 'Members', value: guild.memberCount.toString(), inline: true },
                     { name: 'Roles', value: guild.roles.cache.size.toString(), inline: true },
-                    { name: 'Role List', value: guild.roles.cache.toJSON().join(', ') },
+                    { name: 'Role List', value: guild.roles.cache.toJSON().join(', '), inline: true },
+                    { name: 'Created', value: `<t:${createdTimestamp}:F> (<t:${createdTimestamp}:R>)` }
                 )
-                .setFooter({ text: 'requested by ' + message.author.tag, iconUrl: message.author.displayAvatarURL() })
+                .setFooter({ text: 'requested by ' + message.author.displayName, iconUrl: message.author.displayAvatarURL() })
 
             // message.channel.send(server.commands.toString())
             message.channel.send({ embeds: [ServerEmbed] });
@@ -37,6 +40,5 @@ module.exports = {
             console.log(error);
             message.channel.send(`There was an error Executing \`serverinfo\` command`)
         }
-
     }
 }
