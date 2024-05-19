a1d99df65c3be0adaf41448ddee4c7a717ce94d3
DGamet <jayenrenf@gmail.com>
2024-05-19 20:04:20 +0200

    serverinfo update

--- src/commands/info/serverinfo/sendServerInfo.ts
+++ src/commands/info/serverinfo/sendServerInfo.ts
@@ -1,45 +1,41 @@
-var { EmbedBuilder } = require('discord.js');
+import { GuildMember, Message, EmbedBuilder } from "discord.js";
+
 module.exports = {
-    execute: async (message: any) => {
+    execute: async (message: Message) => {
         try {
-            const guild: any = message.guild;
-            const owner = await guild.fetchOwner();
-            const guildCreatedAt: number = guild.createdAt;
-            const createdTimestamp = (guildCreatedAt / 1000) | 0;
+            if (!message?.guild) {
+                return message.channel.send({ content: `This command can only be executed in a server.` });
+            };
+            const owner = await message.guild.fetchOwner(),
+                createdTimestamp: number = Math.floor(message.guild.createdTimestamp / 1000),
+                channels = message.guild.channels.cache,
+                roles = message.guild.roles.cache.filter(i => i.id !== message.guild?.roles.everyone.id),
+                members = message.guild.members.cache;
+
             const ServerEmbed = new EmbedBuilder()
                 .setColor('#2B2D31')
-                .setTitle(`${guild.name} - Server info`)
-                .setThumbnail(guild.iconURL({ size: 256 }))
-                .setImage(guild.bannerURL({ size: 256 }))
+                .setTitle(`${message.guild.name} - Server info`)
+                .setThumbnail(message.guild.iconURL({ size: 256 }))
+                .setImage(message.guild.bannerURL({ size: 256 }))
                 .addFields(
-                    { name: 'Owner', value: `${owner.user.displayName} (${owner.user.id})`, inline: true },
+                    { name: 'Owner', value: `- ${owner.user.displayName} (${owner.user.id})` },
                     {
-                        name: 'Text Channels',
-                        value: guild.channels.cache.filter((c: any) => c.type === 0).size.toString(),
-                        inline: true,
+                        name: "Channels",
+                        value: `- Text: ${channels.filter((c: any) => c.type === 0).size.toString()}\n- Voice: ${channels.filter((c: any) => c.type === 2).size.toString()}\n- Category: ${channels.filter((c: any) => c.type === 4).size.toString()}`,
                     },
+                    { name: 'Members', value: `- ${members.sort((a, b) => a.joinedTimestamp! - b.joinedTimestamp!).first(10).map(i => `<@${i.id}>`).join(", ")} ${members.size > 15 ? `and **${members.size - 15}** more` : ""}` },
                     {
-                        name: 'Voice Channels',
-                        value: guild.channels.cache.filter((c: any) => c.type === 2).size.toString(),
-                        inline: true,
+                        name: `Roles (${roles?.size ?? "0"} in total)`,
+                        value: `- ${roles.size > 15 ? `${roles.first(15).map((i) => `<@&${i.id}>`).join(", ")} and **${roles?.size - 15}** more` : roles.first(15).map((i) => `<@&${i.id}>`).join(", ")} `,
                     },
-                    {
-                        name: 'Category Channels',
-                        value: guild.channels.cache.filter((c: any) => c.type === 4).size.toString(),
-                        inline: true,
-                    },
-                    { name: 'Members', value: guild.memberCount.toString(), inline: true },
-                    { name: 'Roles', value: guild.roles.cache.size.toString(), inline: true },
-                    { name: 'Role List', value: guild.roles.cache.toJSON().join(', '), inline: true },
-                    { name: 'Created', value: `<t:${createdTimestamp}:F> (<t:${createdTimestamp}:R>)` }
+                    { name: 'Created', value: `- <t:${createdTimestamp}:F> (<t:${createdTimestamp}:R>)` }
                 )
-                .setFooter({ text: 'requested by ' + message.author.displayName, iconUrl: message.author.displayAvatarURL() })
+                .setFooter({ text: 'Requested by ' + message.author.displayName, iconURL: message.author.displayAvatarURL() });
 
-            // message.channel.send(server.commands.toString())
             message.channel.send({ embeds: [ServerEmbed] });
         } catch (error) {
             console.log(error);
-            message.channel.send(`There was an error Executing \`serverinfo\` command`)
-        }
+            message.channel.send(`There was an error while executing \`serverinfo\` command.`)
+        };
     }
-}
+};
