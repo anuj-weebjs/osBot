e61386b144215bca7e5829c7f2cea3716fbd95ba
anuj-weebjs <weeb.js@yahoo.com>
2024-06-28 20:29:06 +0530

    DANGER added kill command for developers onlyy

---
+++ src/commands/danger/kill/kill.ts
@@ -0,0 +1,13 @@
+var config = require('../../../../config.json');
+var developerId = config.developerId;
+module.exports = {
+    execute: async(message: typeof Message)=>{
+        if(message.author.id == developerId){
+            message.channel.send("Shutting down");
+            process.exit(1);
+        }else{
+            return;
+        }
+    },
+
+};
