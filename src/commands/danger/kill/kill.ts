ca578f4c6add0e71ec96012ae0b07b74d1ff1352
core <weeb.js@yahoo.com>
2025-05-29 18:34:23 +0530

    Update kill.ts

--- src/commands/danger/kill/kill.ts
+++ src/commands/danger/kill/kill.ts
@@ -13,7 +13,9 @@ module.exports = {
         if(message.author.id == developerId){
             await message.channel.send("Shutting down");
             client.destroy();
+            process.exit(1);
         }else{
+             message.channel.send("https://tenor.com/view/aww-hell-nahh-aww-hell-aww-hell-nahh-gif-9659330954843262630")
             return;
         }
     },
